1612091e07f8afd8dbfc4aa879a5fa42cef8efe3
DavidHdzHdz <davidhhw24@gmail.com>
2018-06-18 15:22:19 -0500

    starting select company view

--- src/model.js
+++ src/model.js
@@ -12,13 +12,37 @@ const db = firebase.firestore();
 const settings = {/* your settings... */ timestampsInSnapshots: true};
 db.settings(settings);
 
-console.log(myApp.name);
+let companies;
+// convert firebase json to standar json
+const jsonTranspiler = (snapshot) => {
+  let i = 0;
+  let array = [];
+  let object = {};
+  let dataValues = {};
+  snapshot.forEach(data => {
+    object['id'] = data.id;
+    dataValues = data.data();
+    for (const prop in dataValues) {
+      object[prop] = dataValues[prop];
+    }
+    array[i] = object;
+    i++;
+    object = {};
+  });
+  return array;
+}
 
 // getters
 const getUserss = () => {
-  db.collection("people").get().then((querySnapshot) => {
+  db.collection('people').get().then((querySnapshot) => {
     querySnapshot.forEach((doc) => {
       console.log(`${doc.id} => ${doc.data()}`);
     });
+  });
+}
+
+const getCompanies = () => {
+  db.collection('companies').get().then((querySnapshot) => {
+    companies = jsonTranspiler(querySnapshot);
   });
 }
